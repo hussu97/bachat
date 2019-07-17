f736f517e1330a0f6c58602269f708bb4148fbcb
Hussain Abbasi <H_abbasi97@hotmail.com>
2019-07-17 14:13:52 +0400

    refactoring rewards details page

---
+++ lib/components/reward_details/reward_details_content.dart
@@ -0,0 +1,32 @@
+import 'package:bachat/components/reward_details/reward_details_content_bottom.dart';
+import 'package:bachat/components/reward_details/reward_details_content_top.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/reward.dart';
+
+class RewardDetailsContent extends StatelessWidget {
+  final Reward _reward;
+
+  RewardDetailsContent(this._reward);
+
+  @override
+  Widget build(BuildContext context) {
+    LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewPortConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewPortConstraints.maxHeight,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    RewardDetailsContentTop(_reward.backgroundImage, _reward.offer),
+                    RewardDetailsContentBottom(_reward),
+                  ],
+                ),
+              ),
+            );
+          },
+        )
+  }
+}
---
+++ lib/components/reward_details/reward_details_content_top.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import '../../styles.dart';
+
+class RewardDetailsContentTop extends StatelessWidget {
+  final String _backgroundImage;
+  final String _offer;
+  final double _height = 250.0;
+  final Color _opacityOverlay = Color.fromARGB(0x59, 0, 0, 0);
+
+  RewardDetailsContentTop(this._backgroundImage, this._offer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _height,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: FadeInImage.memoryNetwork(
+              image: _backgroundImage,
+              placeholder: kTransparentImage,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Container(
+            color: _opacityOverlay,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              _offer,
+              style: Styles.textDetailsPageTitle,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
